7d028b7edcd600c1c2f9d6f2dcbe2b826165b8dc
heegupark <slhead@gmail.com>
2020-07-20 16:12:02 -0700

    Added some description

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -19,7 +19,7 @@ class Disclaimer extends Component {
           <p className="mx-auto my-3 h4">Welcome to Omega Gram</p>
           <div className="mx-3 my-2 text-center">This app is created strictly for demonstration purposes. By clicking the button below, you accept that Omega Gram do not guarantee storing the notes that you write and the images that you upload.</div>
           <button className="btn btn-sm btn-danger mx-auto my-3" onClick={handleAcceptClick}>Accept</button>
-          <p className="mx-auto my-3 h6 text-warning">This application is under improvement.</p>
+          <p className="mx-auto my-3 text-warning text-center">This application is in-progress. I built this app using React, Node.js, and MongoDB to provide a function for users to create a post with an image and follow others to view their posts.</p>
           <div className="mx-3 my-2 text-center text-secondary">{'If you have any questions, please email to '}<a href="mailto:omegathrone@omegathrone.com">omegathrone@omegathrone.com</a>.</div>
         </div>
       </div>
